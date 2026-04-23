acdc76ab01ce9e0efa66d4468678128f2a4fe6a4
nck90 <s2460@e-mirim.hs.kr>
2026-04-23 12:13:02 +0900

    feat: enable CORS for frontend origin

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'https://seongraekids-client.hyphen.it.com',
+      'http://localhost:5173',
+      'http://localhost:4173',
+    ],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
